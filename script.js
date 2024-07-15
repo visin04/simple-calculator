e88788392d9bcb884cc609d7e3c3c6758efdbc2a
visin04 <168725250+visin04@users.noreply.github.com>
2024-07-16 00:34:00 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+function clearDisplay() {
+    document.getElementById('display').value = '';
+}
+
+function appendToDisplay(value) {
+    document.getElementById('display').value += value;
+}
+
+function calculate() {
+    const display = document.getElementById('display');
+    display.value = eval(display.value);
+}
